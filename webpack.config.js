fe68ab9d1d8670d6fb5157a7a0cd1b266ff9e25b
Ruben Oostinga <ruben.oostinga@gmail.com>
2017-09-08 14:53:25 +0200

    Add webpack bundle analyzer

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,20 @@
 const path = require('path');
+const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer');
 
-module.exports = {
-  entry: {
-    simple: './lib/js/src/simple/simpleRoot.js',
-    interop: './src/interop/interopRoot.js',
-  },
-  output: {
-    path: path.join(__dirname, "bundledOutputs"),
-    filename: '[name].js',
-  },
+module.exports = ({ bundle }) => {
+  const entry = Object.assign(
+    {
+      simple: './lib/js/src/simple/simpleRoot.js',
+    },
+    bundle ? {} : { interop: './src/interop/interopRoot.js' },
+  );
+
+  return {
+    entry,
+    output: {
+      path: path.join(__dirname, 'bundledOutputs'),
+      filename: '[name].js',
+    },
+    plugins: bundle ? [new BundleAnalyzerPlugin()] : [],
+  };
 };
